b91d9d6d6ff0479ad8947194a9ebeb4c54d4de99
GuilhermeAlvesBarros <guilherme.abarros@sptech.school>
2022-11-10 20:35:17 -0300

    java Point

---
+++ java/projeto-point/src/main/java/banco/Database.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco;
+
+import javax.sql.DataSource;
+import javax.swing.Timer;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ *
+ * @author ivanm
+ */
+public class Database {
+    
+   
+ private JdbcTemplate connection;
+
+
+    // Criando o construtor para identificar o banco
+  public Database() {
+
+
+
+    BasicDataSource dataSource = new BasicDataSource();
+
+    dataSource​.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    
+    // Colocar aqui o caminho do banco e o nome do database -- padrão = localhost:3306
+    dataSource​.setUrl("jdbc:sqlserver://bd-point.database.windows.net:1433;database=bd-point;user=adm-point@bd-point;password=1cco#grupo1;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+    
+    // Nome do usuário da conexão 
+    dataSource​.setUsername("adm-point@bd-point");
+    
+    // Senha da conexão 
+    dataSource​.setPassword("1cco#grupo1");
+
+    this.connection = new JdbcTemplate(dataSource);
+
+  }
+  
+  
+  public JdbcTemplate getConnection() {
+
+    return connection;
+
+  }
+  
+
+}
---
+++ java/projeto-point/src/main/java/banco/Main.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco;
+
+import com.github.britooo.looca.api.core.Looca;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import java.io.IOException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author ivanm
+ */
+public class Main {
+
+    public static void main(String[] args) throws UnirestException {
+
+        Looca looca = new Looca();
+
+        Database database = new Database();
+        Utilitarios utilitarios = new Utilitarios();
+
+        Funcionario func = new Funcionario();
+        Maquina maquina = new Maquina();
+        Componente componentes = new Componente();
+        Atributo atributo = new Atributo();
+        Registros registro = new Registros();
+
+
+      
+
+//      
+        if (func.isFuncionarioCadastrado("cleber@point.com", "1234")) {
+
+            System.out.println(maquina.isMaquinaCadastrada(func));
+
+            if (maquina.isMaquinaCadastrada(func)) {
+                System.out.println("Máquina já cadastrada");
+            } else if (maquina.isCadastrarMaquina(func)) {
+                System.out.println("Máquina Cadastrada");
+            } else {
+                System.out.println("Não foi possível encontrar máquina/cadastar máquina");
+            }
+        } else {
+            System.out.println("Funcionário não encontrado");
+        }
+
+        System.out.println("Agora vamos inserir os dados no banco");
+
+        int count = 0;
+
+        while (true) {
+            registro.inserirRegistros(maquina);
+            count++;
+            System.out.println("Foram inseridos " + count + " Dados");
+        }
+
+    }
+}
